6a8bb3776227a86adbc41b203309aea575beda7c
np <nerpoc42@gmail.com>
2025-02-04 10:22:14 +0200

    Added basler camera support

--- camera.py
+++ camera.py
@@ -3,6 +3,7 @@ from deepface import DeepFace
 from pprint import pprint
 import time
 import os
+from pypylon import pylon
 
 backends = [
   'centerface',
@@ -80,44 +81,68 @@ def image_iter(folder_path):
                 print(f'Reading image: {file_path}')
                 yield frame
 
+def basler_camera_iter():
+    camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+    camera.Open()
+
+    # Set auto exposure mode to continuous
+    camera.ExposureAuto.SetValue('Continuous')
+    # Set camera parameters
+    camera.AcquisitionMode.SetValue("Continuous")
+            
+    # Set pixel format to RGB
+    camera.PixelFormat.SetValue("RGB8")
+            
+    camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
+    while camera.IsGrabbing():
+        image = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+        if image.GrabSucceeded():
+            frame = image.Array
+            # frame = cv2.resize(frame,(FRAME_WIDTH, FRAME_HEIGHT))
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            yield frame
+    
+
 # Create a named window and set it to fullscreen
 cv2.namedWindow('Image Display', cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty('Image Display', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-for backend in backends:
-    print(f'Using backend: {backend}')
-    for frame in image_iter("photos"):
-        res, elapsed_time = detect(frame, backend)  # Assuming detect function exists
-        image = draw_face_region(frame, res, backend, elapsed_time)  # Draw on the image
+# for backend in backends:
+#     print(f'Using backend: {backend}')
+#     for frame in image_iter("photos"):
+#         res, elapsed_time = detect(frame, backend)  # Assuming detect function exists
+#         image = draw_face_region(frame, res, backend, elapsed_time)  # Draw on the image
 
-        # Display the resulting frame
-        cv2.imshow('Image Display', image)
+#         # Display the resulting frame
+#         cv2.imshow('Image Display', image)
 
-        # Wait for key press
-        key = cv2.waitKey(0) & 0xFF  # Wait indefinitely until a key is pressed
+#         # Wait for key press
+#         key = cv2.waitKey(0) & 0xFF  # Wait indefinitely until a key is pressed
 
-        # Break loop if 'q' is pressed
-        if key == ord('q'):
-            break
+#         # Break loop if 'q' is pressed
+#         if key == ord('q'):
+#             break
 
-        # Continue to next image only when spacebar is pressed
-        elif key != ord(' '):  # If any other key is pressed, keep waiting
-            continue
+#         # Continue to next image only when spacebar is pressed
+#         elif key != ord(' '):  # If any other key is pressed, keep waiting
+#             continue
 
-# for frame in camera_iter():
-#     backend = 'retinaface'
-#     res, elapsed_time = detect(frame, backend)  # Assuming detect function exists
-#     image = draw_face_region(frame, res, backend, elapsed_time)  # Draw on the image
+for frame in basler_camera_iter():
+    frame = cv2.resize(frame, (640, 480))
 
-#     # Display the resulting frame
-#     cv2.imshow('Image Display', image)
+    backend = 'centerface'
+    res, elapsed_time = detect(frame, backend)  # Assuming detect function exists
+    image = draw_face_region(frame, res, backend, elapsed_time)  # Draw on the image
 
-#     # Wait for key press
-#     key = cv2.waitKey(1) & 0xFF  # Wait indefinitely until a key is pressed
+    # Display the resulting frame
+    cv2.imshow('Image Display', image)
 
-#     # Break loop if 'q' is pressed
-#     if key == ord('q'):
-#         break
+    # Wait for key press
+    key = cv2.waitKey(1) & 0xFF  # Wait indefinitely until a key is pressed
+
+    # Break loop if 'q' is pressed
+    if key == ord('q'):
+        break
 
 
 # Close all OpenCV windows after processing
